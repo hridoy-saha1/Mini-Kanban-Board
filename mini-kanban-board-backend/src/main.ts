67a2cacee38746234ae2acb3dc2153fad07ded66
hridoy8220 <140155955+hridoy8220@users.noreply.github.com>
2026-09-06 11:56:51 +0600

    Deploy Full Project

--- mini-kanban-board-backend/src/main.ts
+++ mini-kanban-board-backend/src/main.ts
@@ -9,7 +9,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000',
+       'https://mini-kanban-board-taupe.vercel.app']
   });
 
   app.useGlobalPipes(
