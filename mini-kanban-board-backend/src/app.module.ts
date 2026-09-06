457ad5a73380629d5a4b8b0504c07f7994e997ca
hridoy8220 <140155955+hridoy8220@users.noreply.github.com>
2026-09-06 10:14:44 +0600

    deploy

--- mini-kanban-board-backend/src/app.module.ts
+++ mini-kanban-board-backend/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { createObserveModule } from '@nestjs/observe';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UserModule } from './User/user.module';
 import { BoardModule } from './Board/board.module';
 import { ColumnModule } from './Column/column.module';
@@ -11,18 +14,25 @@ import { TaskModule } from './Task/task.module';
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
 @Module({
-  imports: [UserModule,BoardModule,ColumnModule,TaskModule,
-    TypeOrmModule.forRoot(
-{ type: 'postgres',
-host: 'localhost',
-port: 5432,
-username: 'postgres',
-password: 'hridoy',
-database: 'kanban_db',
-autoLoadEntities: true,
-synchronize: true,
-} ),
+  imports: [
+    UserModule,
+    BoardModule,
+    ColumnModule,
+    TaskModule,
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+
+      autoLoadEntities: true,
+      synchronize: true,
+
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
   ],
+
   controllers: [AppController],
   providers: [AppService],
 })
